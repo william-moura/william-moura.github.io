f0d2acb7a02a89502549e02c71c03cabab307857
william Moura <williammoura908@gmail.com>
2025-04-21 17:02:12 -0300

    projeto de financas init

---
+++ src/app/list-expenses/list-expenses.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-expenses',
+  imports: [],
+  templateUrl: './list-expenses.component.html',
+  styleUrl: './list-expenses.component.scss'
+})
+export class ListExpensesComponent {
+
+}
