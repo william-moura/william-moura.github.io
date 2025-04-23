e397251de6925764bc478508a156af0e8a52f714
Will_Moura <william@febrafar.com.br>
2025-04-23 15:52:11 -0300

    table

--- src/app/list-expenses/list-expenses.component.ts
+++ src/app/list-expenses/list-expenses.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DataTableComponent } from '../share/data-table/data-table.component';
 
 @Component({
   selector: 'app-list-expenses',
-  imports: [],
+  imports: [DataTableComponent],
   templateUrl: './list-expenses.component.html',
   styleUrl: './list-expenses.component.scss'
 })
 export class ListExpensesComponent {
-
+  columns: string[] = ['id', 'name', 'amount']
+  rows: any[] = [
+    { id: 1, name: 'John Doe', amount: 100 },
+    { id: 2, name: 'Jane Doe', amount: 200 },
+    { id: 3, name: 'Bob Smith', amount: 300 }
+  ]
 }
---
+++ src/app/share/data-table/data-table.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-data-table',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './data-table.component.html',
+  styleUrl: './data-table.component.scss'
+})
+export class DataTableComponent {
+  @Input() rows!: any[];
+  @Input() columns!: string[];
+}
