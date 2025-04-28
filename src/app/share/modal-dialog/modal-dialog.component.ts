ce5babe9d5c3a4dd73ab97e6cddf18e8f1366e4a
Will_Moura <william@febrafar.com.br>
2025-04-28 16:56:50 -0300

    criando modal ainda cru

---
+++ src/app/share/modal-dialog/modal-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-dialog',
+  imports: [],
+  templateUrl: './modal-dialog.component.html',
+  styleUrl: './modal-dialog.component.scss'
+})
+export class ModalDialogComponent {
+
+}
