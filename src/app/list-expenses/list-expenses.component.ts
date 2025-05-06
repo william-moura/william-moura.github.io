062c481b025602731cd193db44a8a5cd2c271fe9
Will_Moura <william@febrafar.com.br>
2025-05-06 17:06:08 -0300

    atualização

--- src/app/list-expenses/list-expenses.component.ts
+++ src/app/list-expenses/list-expenses.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { DataTableComponent } from '../share/data-table/data-table.component';
+import { ModalDialogComponent } from "../share/modal-dialog/modal-dialog.component";
 
 @Component({
   selector: 'app-list-expenses',
-  imports: [DataTableComponent],
+  imports: [DataTableComponent, ModalDialogComponent],
   templateUrl: './list-expenses.component.html',
   styleUrl: './list-expenses.component.scss'
 })
@@ -14,4 +15,9 @@ export class ListExpensesComponent {
     { id: 2, name: 'Jane Doe', amount: 200 },
     { id: 3, name: 'Bob Smith', amount: 300 }
   ]
+  objeto: object = {};
+
+  public addExpense() {
+    console.log('Adicionar despesa');
+  }
 }
--- src/app/share/modal-dialog/modal-dialog.component.ts
+++ src/app/share/modal-dialog/modal-dialog.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, output, Output } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-modal-dialog',
-  imports: [],
+  imports: [ReactiveFormsModule],
+  standalone: true,
   templateUrl: './modal-dialog.component.html',
   styleUrl: './modal-dialog.component.scss'
 })
 export class ModalDialogComponent {
-
+  // formModel = new FormControl('');
+  eventEmitter = new EventEmitter()
+  @Output() formModel :any = {
+    description: new FormControl(''),
+    value: new FormControl(''),
+    dueDate: new FormControl('')
+  }
+  public addExpense() {
+    console.log(this.formModel,'Adicionar despesa');
+    this.eventEmitter.emit(this.formModel);
+  }
 }
